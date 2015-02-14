aa529d108c0e153dd3d737f1310611b87836f780
john@drkurt.com <john@drkurt.com>
2015-02-14 09:08:32 -0800

    javascript

--- app/static/js/jsForBaseTmpTable.js
+++ app/static/js/jsForBaseTmpTable.js
@@ -9,19 +9,17 @@ $('div.box').hover(function(e) {
 	
 	var num = parseInt($(this).attr('data-popbox'));
 	var target = '#' + ($(this).attr('data-popbox'));
- 	var name = document.getElementById(num).innerHTML;
-	//name = name.slice(0,15)
-	//alert('innerhtml : ' + name.slice(0,16));
-
+ 	//var name = document.getElementById(num).innerHTML;
+	
 	moveLeft = $(this).outerWidth();
 	moveDown = ($(target).outerHeight()-50);
 	
 
 	if (num >= 400) {
 		$(target).css('top', 0).css('left', 0);
-	} else if (name.slice(0,16) === "M3AAWG Night Out") {
+	} /*else if (name.slice(0,16) === "M3AAWG Night Out") {
 		$(target).css('top', -moveDown).css('left', moveLeft);
-	} else if (num%10 === 0) {
+	} */else if (num%10 === 0) {
 		$(target).css('top', 0).css('left', moveLeft);
 	} else {
 		$(target).css('top', -moveDown).css('left', moveLeft);
