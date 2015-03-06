55727cedc078356786c67ce56124888300709d37
john@drkurt.com <john@drkurt.com>
2015-03-06 09:40:55 -0800

    auto update

--- fabfile.py
+++ fabfile.py
@@ -10,17 +10,20 @@ env.user = 'ec2-user'
 def prepare_deploy():
 	local("gunicorn manage:app")
 	
+def webServerUpdate():
+	code_dir = '/home/ec2-user/m3aawgApp.git/app'
+	run("source venv/bin/activate")
+	run("pkill gunicorn")
+	run("gunicorn manage:app")
+
 def updateApp():
 	local("git add . && git commit -m 'auto update'")
 	local("git push web master")
 	
-
 def deploy():
 	code_dir = '/home/ec2-user/m3aawgApp.git/app'
-		
 	updateApp()
 	with cd(code_dir):
-
 		run("source venv/bin/activate")
 		run("pkill gunicorn")
 		run("sudo pip install -r requirements.txt")
